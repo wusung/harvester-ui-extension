2f62376e6a8eee82ab98b19c908bbe5949d767e9
wusung <twkoci@gmail.com>
2026-02-05 16:25:53 +0800

    fix: workflow environment variables and job dependencies
    - Add global env variables for default versions
    - Replace hardcoded values with environment references
    - Use --amend flag for git commits
    - Ensure all job dependencies defined

---
+++ pkg/harvester/dynamic-importer.js
@@ -0,0 +1,2 @@
+// Re-export with explicit .js to avoid library dynamic-importer replacement.
+export * from '@rancher/shell/utils/dynamic-importer.js';
--- pkg/harvester/vue.config.js
+++ pkg/harvester/vue.config.js
@@ -1,1 +1,16 @@
-module.exports = require('@rancher/shell/pkg/vue.config')(__dirname);
+const config = require('@rancher/shell/pkg/vue.config')(__dirname);
+const path = require('path');
+
+module.exports = {
+  ...config,
+  configureWebpack: (webpackConfig) => {
+    if (typeof config.configureWebpack === 'function') {
+      config.configureWebpack(webpackConfig);
+    }
+
+    // Ensure drawer imports are available in library builds.
+    webpackConfig.resolve = webpackConfig.resolve || {};
+    webpackConfig.resolve.alias = webpackConfig.resolve.alias || {};
+    webpackConfig.resolve.alias['@shell/utils/dynamic-importer'] = path.join(__dirname, 'dynamic-importer.js');
+  }
+};
